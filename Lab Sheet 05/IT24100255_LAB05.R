4048e3f395d4c773c34d9e47fc64162205cea2bd
it24100255 <it24100255@my.sliit.lk>
2025-08-26 21:48:23 +0530

    Add files via upload

---
+++ Lab Sheet 05/IT24100255_LAB05.R
@@ -0,0 +1,20 @@
+setwd("C:\\Users\\Dell\\OneDrive\\Desktop\\Labo5")
+getwd()
+
+#01
+Delivery_Times<-read.table("Exercise - Lab 05.txt",header=TRUE)
+head(Delivery_Times)
+attach(Delivery_Times)
+
+#02
+breaks<-seq(20,70,by=(70-20)/9)
+hist(Delivery_Time_.minutes.,main = "Delivery Time(Minutes)",breaks=breaks,right=TRUE)
+
+#03
+#The curve shows a bimodal distribution and appears approximately symmetrical. The data spans between 20 to 70 minutes.
+
+#04
+freq_table<-hist(Delivery_Time_.minutes.,breaks=breaks,)
+cum_freq<-cumsum(freq_table$counts)
+
+plot(freq_table$mids,cum_freq,type="o",main="cumulative Frequency Polygon",xlab="Delivery Time", ylab="Cumulative Frequency")
