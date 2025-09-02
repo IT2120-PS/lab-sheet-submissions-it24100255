4193c43c9d6f41baefbd7be1912fd2e865922b1f
it24100255 <it24100255@my.sliit.lk>
2025-09-02 16:22:35 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24100255_LAB06.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\it24100255\\Desktop\\IT2400255")
+getwd()
+#1)
+#i.Bionomial distribution
+#ii.
+1-pbinom(46,50,0.85,lower.tail=TRUE)
+
+
+#2)
+#i. Number of calls per hour.
+#ii. Poisson distribution
+#iii.
+dpois(15,20)
+
